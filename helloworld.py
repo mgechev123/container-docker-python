75a0f2ad7533810921b2fec2a6e66bd5d6b71c3d
mgechev123 <72758767+mgechev123@users.noreply.github.com>
2020-10-13 19:04:46 -0300

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -6,7 +6,7 @@ api = Api(app)
 
 class Greeting (Resource):
     def get(self):
-        return '<p>one paragraph<br />with a line break in the middle</p>'"a""""
+        return '<p>one paragraph<br />with a line break in the middle</p>'
 
 api.add_resource(Greeting, '/') # Route_1
 
