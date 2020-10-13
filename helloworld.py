6b697215212c468b584183bf0991d32e5a76407d
mgechev123 <72758767+mgechev123@users.noreply.github.com>
2020-10-13 18:54:10 -0300

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -6,8 +6,7 @@ api = Api(app)
 
 class Greeting (Resource):
     def get(self):
-        return ("""Hello World!
-                Imagem docker gerada a partir do arquivo Dockerfile funcionou corretamente!
+        return ("""Hello World!<br/>Imagem docker gerada a partir do arquivo Dockerfile funcionou corretamente!
                 Atividade pratica PUC - Containers""")
 
 api.add_resource(Greeting, '/') # Route_1
